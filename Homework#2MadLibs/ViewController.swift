c2658079dbe055644dc310fb275112d8a74aba46
Mia Willett <willettfamilyoc@mac.com>
2015-07-01 13:51:00 -0500

    Initial Commit

---
+++ Homework#2MadLibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework#2MadLibs
+//
+//  Created by mia willett on 7/1/15.
+//  Copyright © 2015 mia willett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
